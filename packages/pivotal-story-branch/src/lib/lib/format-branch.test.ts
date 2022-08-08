f61588aa8fe663280acdf7a9d7dd9b99fa6b0408
Jon Penton <hey@jondpenton.com>
2022-08-08 20:53:35 +0000

    test(pivotal-story-branch): :white_check_mark: Fixed test errors

--- packages/pivotal-story-branch/src/lib/lib/format-branch.test.ts
+++ packages/pivotal-story-branch/src/lib/lib/format-branch.test.ts
@@ -26,10 +26,10 @@ it('formats a long story correctly', () => {
 })
 
 it('returns a longer formatted branch name when PIVOTAL_TRACKER_BRANCH_MAX_LENGTH is set', () => {
-  const originalMaxLength = process.env.PIVOTAL_TRACKER_BRANCH_MAX_LENGTH
+  const originalMaxLength = process.env['PIVOTAL_TRACKER_BRANCH_MAX_LENGTH']
 
   try {
-    process.env.PIVOTAL_TRACKER_BRANCH_MAX_LENGTH = '60'
+    process.env['PIVOTAL_TRACKER_BRANCH_MAX_LENGTH'] = '60'
     const story: IStory = {
       id: 1,
       name: "this is an extremely, long, precarious name and should be shortened but won't",
@@ -42,7 +42,7 @@ it('returns a longer formatted branch name when PIVOTAL_TRACKER_BRANCH_MAX_LENGT
       'feature/this-is-an-extremely-long-precarious-name-and-#1',
     )
   } catch (err) {
-    process.env.PIVOTAL_TRACKER_BRANCH_MAX_LENGTH = originalMaxLength
+    process.env['PIVOTAL_TRACKER_BRANCH_MAX_LENGTH'] = originalMaxLength
 
     throw err
   }
